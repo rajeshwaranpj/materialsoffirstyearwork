fcd8a66568181fd183aa06ba17f1b9066b3c38cd
rajeshwaranpj <rajeshwaranpj@gmail.com>
2016-06-22 11:42:13 +0000

    Add files via upload

---
+++ assignment/othersAssignmnet/akWeek2Assignment3/Week2Assignment3/script/survey.js
@@ -0,0 +1,16 @@
+﻿function validate()
+{
+    var a = document.getElementById("internal").value;
+    var b = document.getElementById("type").value;
+    var c = document.getElementById("date").value;
+    var d = document.getElementById("language").value;
+    var e = document.getElementById("survey").value;
+    var f = document.getElementById("description").value;
+
+    if (b == "" || c == "" || d == "" || e == "" || f == "" || a == "") {
+        alert("Fill In Required Fields");
+    }
+
+    else
+        alert("Submitted");
+}
